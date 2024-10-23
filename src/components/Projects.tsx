92a1cd2f4e88c8148330dbc529ce0e8e167d7e8d
Maha Faisal <126847755+MahaaFaisal@users.noreply.github.com>
2024-10-23 07:17:46 +0000

    link to qualification added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { FolderGit2 } from 'lucide-react';
+// import { FolderGit2 } from 'lucide-react';
 
 interface ProjectsProps {
   scrollY: number;
@@ -23,13 +23,33 @@ const Projects: React.FC<ProjectsProps> = ({ scrollY }) => {
 
   const projects = [
     {
+      title: "libft",
+      description: "Libft is the first project in the 42 school curriculum, where I am tasked with creating my own C library. The project involves implementing a variety of functions from the standard C library, building a solid foundation in understanding how these functions work internally.Graduation project focused on creating a comprehensive dashboard for startup performance monitoring.",
+      technologies: ["C language", "Makefile", "Valgrind"],
+    },
+    {
+      title: "minishell",
+      description: "Minishell is a challenging group project in the 42 school curriculum, where I am building a Unix-like shell in C that mimics the functionality of Bash. The shell must handle complex tasks such as parsing and executing commands, managing processes and signals, handling pipes, redirections, environment variables, and more. The goal is to replicate core Bash behavior, making the project both an in-depth exploration of system programming and process management.",
+      technologies: ["C language", "Makefile", "Valgrind", "bash", "System calls", "Git"],
+    },
+    {
+      title: "Philosophers",
+      description: "The Philosophers project is a key part of the 42 school curriculum, where I simulate the Dining Philosophers problem in C.This project delves into concurrency and synchronization, requiring the management of multiple threads representing philosophers who alternate between thinking and eating. The challenge involves ensuring proper allocation and release of resources (forks) to avoid deadlock and starvation, showcasing a comprehensive understanding of multithreading.",
+      technologies: ["C language", "Makefile", "Valgrind", "bash", "System calls", "Git"],
+    },
+    {
+      title: "Cub3d",
+      description: "The Cub3D group project is an integral part of the 42 school curriculum, where I am tasked with developing a 3D game engine that renders a virtual environment using raycasting techniques. This project involves implementing complex algorithms to simulate the behavior of light and create a visually immersive experience. My work focuses on creating an efficient rendering pipeline, handling user inputs, and optimizing performance to ensure a smooth gameplay experience.",
+      technologies: ["C language", "Makefile", "Valgrind", "MiniLibx", "System calls", "Git"],
+    },
+    {
       title: "Multi-Level Business Performance Monitoring Dashboard for Startups",
-      description: "Graduation project focused on creating a comprehensive dashboard for startup performance monitoring.",
-      technologies: ["React", "Node.js", "MongoDB"],
+      description: "My graduation project involves developing a multi-level business performance monitoring dashboard specifically designed for startups. This comprehensive dashboard utilizes Power BI to visualize key performance indicators (KPIs), allowing stakeholders to track and analyze essential metrics related to growth, financial health, and operational efficiency. The goal is to provide actionable insights that empower startup founders to make informed decisions and drive strategic initiatives.",
+      technologies: ["Data Visualization", "PowerBI", "DAX", "MongoDB"],
     },
     {
       title: "3D Printer Project",
-      description: "Contributed to the development of a 3D printer that builds 3-dimensional objects.",
+      description: "I contributed to the development of a 3D printer capable of producing three-dimensional objects. This project involved a multidisciplinary approach, integrating 3D modeling, electronics, and mechanical engineering principles. My role focused on designing and implementing the mechanical components and ensuring the seamless integration of electronic systems, ultimately aiming to enhance the printer's performance and reliability.",
       technologies: ["3D Modeling", "Electronics", "Mechanical Engineering"],
     },
   ];
@@ -44,7 +64,7 @@ const Projects: React.FC<ProjectsProps> = ({ scrollY }) => {
             <p className="mb-4 text-gray-600">{project.description}</p>
             <div className="flex flex-wrap gap-2">
               {project.technologies.map((tech, idx) => (
-                <span key={idx} className="bg-blue-100 text-blue-800 px-2 py-1 rounded-full text-sm">
+                <span key={idx} className="bg-gray-200 text-brown-800 px-3 py-1 rounded-lg text-sm">
                   {tech}
                 </span>
               ))}
--- src/components/Qualifications.tsx
+++ src/components/Qualifications.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useRef } from 'react';
-import { GraduationCap } from 'lucide-react';
-
+// import { GraduationCap } from 'lucide-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import certificate from '/workspaces/Maha-Gasm-Elsid-professional-portfolio/src/resources/Certificate.pdf';
+import transcript42 from '/workspaces/Maha-Gasm-Elsid-professional-portfolio/src/resources/transcript42.pdf';
 interface QualificationsProps {
   scrollY: number;
 }
@@ -24,19 +27,34 @@ const Qualifications: React.FC<QualificationsProps> = ({ scrollY }) => {
   return (
     <section id="qualifications" className="py-16 section-enter" ref={sectionRef}>
       <h2 className="text-3xl font-bold mb-8 text-gray-800">Qualifications</h2>
+
       <div className="space-y-6">
+
         <div className="card p-6">
-          <h3 className="text-xl font-semibold mb-2 text-gray-800">Software Engineering</h3>
-          <p className="text-gray-600">42 Abu Dhabi, UAE</p>
-          <p className="text-sm text-gray-500">OCTOBER 2023 - PRESENT</p>
-        </div>
+        <h3 className="text-xl font-semibold mb-2 text-gray-800">
+          Diploma in Software Development
+          <a href={transcript42} target="_blank" rel="noopener noreferrer" className="ml-2">
+            <FontAwesomeIcon icon={faLink} className="text-gray-400" />
+          </a>
+        </h3>
+        <p className="text-gray-600">42 Abu Dhabi, UAE</p>
+        <p className="text-sm text-gray-500">OCTOBER 2023 - PRESENT</p>
+        <p className="mt-2">GPA: 3.67/4.00</p>
+        <p className="mt-2 text-gray-700">Graduation Project: "Multi-Level Business Performance Monitoring Dashboard for Startups."</p>
+      </div>
+
         <div className="card p-6">
-          <h3 className="text-xl font-semibold mb-2 text-gray-800">Bachelor of Science in Information Technology</h3>
-          <p className="text-gray-600">University of Khartoum, Sudan</p>
-          <p className="text-sm text-gray-500">JANUARY 2023</p>
-          <p className="mt-2">GPA: 3.67/4.00</p>
-          <p className="mt-2 text-gray-700">Graduation Project: "Multi-Level Business Performance Monitoring Dashboard for Startups."</p>
-        </div>
+        <h3 className="text-xl font-semibold mb-2 text-gray-800">
+          Bachelor of Science in Information Technology
+          <a href={certificate} target="_blank" rel="noopener noreferrer" className="ml-2">
+            <FontAwesomeIcon icon={faLink} className="text-gray-400" />
+          </a>
+        </h3>
+        <p className="text-gray-600">University of Khartoum, Sudan</p>
+        <p className="text-sm text-gray-500">JANUARY 2023</p>
+        <p className="mt-2">GPA: 3.67/4.00</p>
+        <p className="mt-2 text-gray-700">Graduation Project: "Multi-Level Business Performance Monitoring Dashboard for Startups."</p>
+      </div>
       </div>
     </section>
   );
